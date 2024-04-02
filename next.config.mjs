b2a02a7aee2c75844ce77e77e6d1cc1d0d2d4325
Pushpendra Pal <p_pal@bt.iitr.ac.in>
2024-04-03 05:23:12 +0530

    headers added in nextjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: false,
+  reactStrictMode: false,
+  //   async headers() {
+  //     return [
+  //       {
+  //         source: "/(.*)",
+  //         headers: [{ key: "Cross-Origin-Opener-Policy", value: "http://localhost:3000" }],
+  //       },
+  //     ];
+  //   },
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin-allow-popups", // "same-origin-allow-popups"
+          },
+        ],
+      }
+    ];
+  },
 };
 
 export default nextConfig;
